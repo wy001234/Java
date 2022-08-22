9b1f15b752d8663ff50acebf6823be6ed21c2972
qpdlxm145 <qpdlxm145@gmail.com>
2022-08-22 10:34:04 +0900

    2022/08/22

---
+++ Ch04/src/Sub01/ArrayTest.java
@@ -0,0 +1,54 @@
+package Sub01;
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 설우영
+ * 내용 : Java 배열 실습하기
+ * 
+ * 배열(Array)
+ * - 하나이상의 데이터를 하나의 변수에 저장할 수 있는 자료구조
+ * - 현대 프로그램에서는 배열 대신 리스트 List()를 더 많이 사용
+ */
+public class ArrayTest {
+	public static void main(String[] args) {
+		
+		//배열
+		
+		int arr1[] = {1,2,3,4,5};
+		char[] arr2 = {'A','B','C'};
+		String[] arr3 = {"서울","대전","대구","부산","광주"};
+		
+		
+		//배열 원소 출력
+		System.out.println("arr1[] : "+ arr1[0]);
+		System.out.println("arr1[2] : "+ arr1[2]);
+		System.out.println("arr1[4] : "+ arr1[4]);
+		System.out.println("arr2[0] : " + arr2[0]);
+		System.out.println("arr2[1] : " + arr2[1]);
+		System.out.println("arr3[0] : " + arr3[0]);
+		System.out.println("arr3[3] : " + arr3[3]);
+		System.out.println("arr3[4] : " + arr3[4]);
+		
+		//배열 길이 = 원소의 개수
+		System.out.println("arr1의 배열의 길이" + arr1.length);
+		System.out.println("arr2의 배열의 길이" + arr2.length);
+		System.out.println("arr3의 배열의 길이" + arr3.length);
+	
+		
+		//배열 반복문
+		for(int i=0; i<arr1.length; i++) {
+			System.out.println("arr1["+i+"] :" + arr1[i]);
+			
+		}
+		
+		for(char ch: arr2) {
+			System.out.print(ch + "");
+		}
+		System.out.println();
+		
+		for(String city: arr3) {
+			System.out.printf("%s", city); //printf = 형식
+		}
+		
+	}
+	
+}
